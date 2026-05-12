c504c31538633b49e852ef200eb8629cee88527b
markdeprez-netizen <mark.deprez@gmail.com>
2026-05-12 18:10:23 +0200

    Update streamlit_app.py
    changes required moving connection from SiS to SnIS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 session = get_active_session()
 
