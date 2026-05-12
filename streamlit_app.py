00e6abfc7bea16a0bfdb540a16a530f74a593e6e
markdeprez-netizen <mark.deprez@gmail.com>
2026-05-12 19:36:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,14 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-import pandas as pd
-
 if ingredients_list:
   ingredients_string = ''
   
   for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
-    st.subheader(f"{fruit_chosen} Nutrition Information")
-    response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
-    data = response.json()
-    df = pd.json_normalize(data)
-    st.dataframe(df, use_container_width=True)
+    st.subheader(fruit_chosen + ' Nutrition Information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
 st.write(ingredients_string)
 
@@ -56,9 +52,6 @@ if time_to_insert:
 
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 
     
 
