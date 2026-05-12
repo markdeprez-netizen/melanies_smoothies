be97ce72c015fae18960ff9026545bd1a1ba86ce
markdeprez-netizen <mark.deprez@gmail.com>
2026-05-12 20:05:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
+
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
 
 cnx = st.connection("snowflake")
 session = cnx.session()
