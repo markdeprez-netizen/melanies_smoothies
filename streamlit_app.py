88d0f4d9a63bf22e1491050ed2dea3c7f674d1ec
markdeprez-netizen <mark.deprez@gmail.com>
2026-05-12 19:20:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
 if ingredients_list:
   ingredients_string = ''
   
-    for fruit_chosen in ingredients_list:
+  for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
       st.subheader(fruit_chosen + ' Nutrition Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
